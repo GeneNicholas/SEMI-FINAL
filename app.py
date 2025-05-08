1f13015cf10ca103fb10dc3617ce27d42fc56599
GeneNicholas <madrona.genenicholas@cec.edu.ph>
2025-05-08 09:48:32 +0800

    Third Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +9,7 @@ def home():
             ' 1st 25'
             ' System Integration'
             ' Semi-Final Exam </h1>')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
